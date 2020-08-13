f8dcc66e64f05246a3d0e2abe36fa480f452a063
czq <1150118968@qq.com>
2020-08-13 14:12:50 +0800

    主体页面绘制
    Signed-off-by: czq <1150118968@qq.com>

--- app/src/main/java/com/example/transportclient/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/transportclient/ui/main/PlaceholderFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.transportclient.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -46,6 +52,52 @@ public class PlaceholderFragment extends Fragment {
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
         final TextView textView = root.findViewById(R.id.section_label);
+
+        ListView listView_wfs = root.findViewById(R.id.list_wfs);
+        ArrayList<Map<String, Object>> arr_data = new ArrayList<>();
+        // 新增数据
+        for (int i5 = 0; i5 < 20; i5++) {
+            Map map = new HashMap<String, Object>();
+            //map放入两个键值对，键名与from对应，
+            map.put("pn", "13131313" + i5);
+
+            //往list添加数据
+            arr_data.add(map);
+        }
+
+        // 新建适配器 ，绑定数据
+        String[] from = {"pn"};
+        int[] to = {R.id.phone_number};
+        SimpleAdapter simp_ada = new SimpleAdapter(getContext(), arr_data, R.layout.list_fs_item, from, to);
+
+        listView_wfs.setAdapter(simp_ada);
+
+
+        View root2 = inflater.inflate(R.layout.fragment_detail, container, false);
+        //final TextView textView = root.findViewById(R.id.section_label);
+
+        ListView listView_fs = root2.findViewById(R.id.list_fs);
+
+
+        ArrayList<Map<String, Object>> arr_data2 = new ArrayList<>();
+        // 新增数据
+        for (int i5 = 0; i5 < 20; i5++) {
+            Map map = new HashMap<String, Object>();
+            //map放入两个键值对，键名与from对应，
+            map.put("pn", "13131313" + i5);
+
+            //往list添加数据
+            arr_data.add(map);
+        }
+
+        // 新建适配器 ，绑定数据
+        String[] from2 = {"pn"};
+        int[] to2 = {R.id.phone_number};
+        SimpleAdapter simp_ada2 = new SimpleAdapter(getContext(), arr_data2, R.layout.list_fs_item, from, to);
+
+        listView_fs.setAdapter(simp_ada2);
+
+
 //        pageViewModel.getText().observe(getContext(), new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
